e01092937e3f70c014768e5c0af471835aefe115
john8680 <zhang8680@outlook.com>
2017-07-05 23:48:02 +0800

    register

--- member/urls.py
+++ member/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     url(r'^login/',views.LoginView.as_view(),name='member_login'),
     url(r'^logout/',views.LogoutView.as_view(), name='member_logout'),
-    url(r'^resiter/', views.RegisterView.as_view(), name='member_register'),
+    url(r'^logout/(?P<next>register|login)', views.LogoutView.as_view(),name='member_logout'),
+    url(r'^register/', views.RegisterView.as_view(), name='member_register'),
 ]
--- member/views.py
+++ member/views.py
@@ -6,10 +6,11 @@ from django.shortcuts import render,HttpResponseRedirect,reverse
 from django.views.generic import View,TemplateView,FormView
 from utils.message_box import MessageBox
 from member.models import Member
-from member.forms import LoginForm
+from member.forms import LoginForm,RegisterForm
 
 # Create your views here.
 
+#TODO: 将url写入环境变量方便管理
 class LoginView(FormView):
     failed_url = '/member/login'
     success_url = '/novel'
@@ -19,11 +20,10 @@ class LoginView(FormView):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
-            self.messages.success(request,u'您已经登录,<a href="%s">切换其他账户?</a>'%(reverse('member:member_logout')))
-            return HttpResponseRedirect('/novel')
+            self.messages.success(request,u'您已经登录,<a href="%s">切换其他账户?</a>' % (reverse('member:member_logout')))
+            return HttpResponseRedirect(self.success_url)
         else:
-            form=LoginForm()
-            return render(request,self.template_name, {'form':form})
+            return render(request,self.template_name, {'form':self.form_class})
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
@@ -48,18 +48,56 @@ class LoginView(FormView):
 
 
 class LogoutView(TemplateView):
-    redirect_url = '/member/login'
+    redirect_url = '/member/'
     messages=MessageBox()
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
-            self.messages.success(request,u'注销成功，请重新登录')
-            logout(request)
-            return HttpResponseRedirect(redirect_to=self.redirect_url)
+            if request.GET.has_key('next') and request.GET.get('next')=='register':
+                self.messages.success(request,u'注销成功')
+                logout(request)
+                return HttpResponseRedirect(redirect_to=self.redirect_url+'register')
+            else:
+                self.messages.success(request,u'注销成功，请重新登录')
+                logout(request)
+                return HttpResponseRedirect(redirect_to=self.redirect_url+'login')
         else:
             self.messages.warning(request,u'您还未登录,请登录')
             return HttpResponseRedirect(redirect_to=self.redirect_url)
 
 
 class RegisterView(FormView):
-    pass
+    messages=MessageBox()
+    logout_url='/member/logout/?next=register'
+    register_url='/member/register'
+    template_name = 'register.html'
+    form_class = RegisterForm
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            self.messages.warning(request,u'您 %s 已经登录,请先<a href="%s">注销</a>'%(request.user,self.logout_url))
+            return render(request,'register.html',{'form':self.form_class})
+        else:
+            return render(request,'register.html',{'form':self.form_class})
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        print form.is_valid()
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            email=form.cleaned_data['email']
+            confirm=form.cleaned_data['confirm_password']
+            if confirm==password:
+                if Member.objects.filter(username__exact=username).exists():
+                    self.messages.warning(request,u'用户名已存在')
+                    return render(request, 'register.html', {})
+                else:
+                    if Member.objects.filter(email__exact=email).exists():
+                        self.messages.warning(request,u'邮箱已存在')
+                        return render(request, 'register.html', {})
+                    else:
+                        user=Member.objects.create_user(username=username,email=email,password=password)
+                        self.messages.success(request,u'注册成功!')
+                        return render(request,'register.html',{'user':user})
+            else:
+                self.messages.warning(request,u'两次密码输入不一致')
+                return render(request,'register.html',{})
 
